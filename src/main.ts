70a475962b4b1b6cab4ce0ab1d0449e8654654bb
Zeeshan Yousaf <zeeshan.yousaf@gagamuller.com>
2024-06-06 16:14:39 +0500

    bugs fuze-template

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from 'app/app.component';
 import { appConfig } from 'app/app.config';
 import { registerLicense } from '@syncfusion/ej2-base';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 registerLicense('ORg4AjUWIQA/Gnt2UFhhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTX5Wd0BjW3xecHxTQmdY');
 
